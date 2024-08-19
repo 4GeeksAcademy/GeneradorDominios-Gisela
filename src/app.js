55aa0a17f4d3f751f1059571d03a2b525b0ff64f
Gisela94 <gisela.garrido@hotmail.com>
2024-08-19 19:01:56 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -37,12 +37,13 @@ window.onload = function() {
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let domainResult = [];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-      console.log(pronoun[i] + adj[j] + noun[k]);
+      domainResult.push(pronoun[i] + adj[j] + noun[k] + ".com");
     }
   }
 }
-
+console.log(domainResult);
